318dde2078c40d11e2f1f61dc0fde524b1c77974
yusixian <cosine_yu@qq.com>
2024-04-17 21:14:56 +0800

    refactor: add jotai and toolPanelOpenAtom

--- components/ExportFollowers.tsx
+++ components/ExportFollowers.tsx
@@ -1,14 +1,15 @@
 import Button from "antd/es/button"
 import Card from "antd/es/card"
-import { AnimatePresence, motion } from "framer-motion"
+import { useSetAtom } from "jotai"
 import { useCallback, useMemo, useState } from "react"
-import { FaChevronLeft, FaTools } from "react-icons/fa"
-import { FaXTwitter } from "react-icons/fa6"
+import { FaChevronLeft } from "react-icons/fa"
 import { useCopyToClipboard } from "react-use"
+
+import { toolPanelOpenAtom } from "~store/app"
 
 const ExportFollowers = () => {
   const [list, setList] = useState<string[]>([])
-  const [open, setOpen] = useState<boolean>(false)
+  const setOpen = useSetAtom(toolPanelOpenAtom)
 
   const nextBatch = useCallback(() => {
     console.log("下一批！")
@@ -49,6 +50,7 @@ const ExportFollowers = () => {
   const setTimer = () => {
     setTimeout(() => {
       if (isScrollAtBottom()) {
+        // <FIXME>judge scroll bottom</FIXME>
         console.log("到底了")
         return
       }
@@ -68,63 +70,38 @@ const ExportFollowers = () => {
   const [_, copy] = useCopyToClipboard()
 
   return (
-    <AnimatePresence mode="sync">
-      {open ? (
-        <motion.div
-          key="panel-open"
-          layoutId="followers-panel"
-          initial={{ x: -100 }}
-          animate={{ x: 0 }}
-          exit={{ x: -100 }}
-          transition={{ duration: 0.5, ease: "linear" }}
-          className="w-80 fixed left-0 top-8">
-          <Card className="px-3 rounded-ss-none rounded-es-none">
-            <div
-              className="absolute transition rounded-se-lg rounded-es-lg cursor-pointer bg-[#141414] px-2 inset-y-0 right-0 flex justify-center items-center hover:brightness-50"
-              onClick={() => {
-                setOpen(false)
-              }}>
-              <FaChevronLeft />
-            </div>
-            <div className="flex gap-3 flex-col">
-              <Button type="primary" onClick={getAllUsername}>
-                Get All Username (Next Batch)
-              </Button>
-              <Button type="primary" onClick={() => copy(listStr)}>
-                Copy All
-              </Button>
-              <Button
-                onClick={() => {
-                  setList([])
-                  window.scrollTo({
-                    top: 0
-                  })
-                }}>
-                Clear
-              </Button>
-            </div>
-            <p className="my-2 text-lg">Count {list?.length ?? 0}</p>
-            <textarea
-              className="overflow-auto h-96 w-full p-2"
-              value={listStr}
-              readOnly
-            />
-          </Card>
-        </motion.div>
-      ) : (
-        <motion.div
-          key="panel-close"
-          layoutId="followers-panel"
-          className="fixed left-0 top-8">
-          <Button
-            className="flex h-auto py-3 rounded-ss-none rounded-es-none px-2 flex-col gap-2"
-            onClick={() => setOpen(true)}>
-            <FaXTwitter size={22} />
-            <FaTools size={22} />
-          </Button>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <Card className="px-3 rounded-ss-none rounded-es-none">
+      <div
+        className="absolute transition rounded-se-lg rounded-es-lg cursor-pointer bg-[#141414] px-2 inset-y-0 right-0 flex justify-center items-center hover:brightness-50"
+        onClick={() => {
+          setOpen(false)
+        }}>
+        <FaChevronLeft />
+      </div>
+      <div className="flex gap-3 flex-col">
+        <Button type="primary" onClick={getAllUsername}>
+          Get All Username (Next Batch)
+        </Button>
+        <Button type="primary" onClick={() => copy(listStr)}>
+          Copy All
+        </Button>
+        <Button
+          onClick={() => {
+            setList([])
+            window.scrollTo({
+              top: 0
+            })
+          }}>
+          Clear
+        </Button>
+      </div>
+      <p className="my-2 text-lg">Count {list?.length ?? 0}</p>
+      <textarea
+        className="overflow-auto h-96 w-full p-2"
+        value={listStr}
+        readOnly
+      />
+    </Card>
   )
 }
 
---
+++ content.tsx
@@ -0,0 +1,66 @@
+import { StyleProvider } from "@ant-design/cssinjs"
+import Button from "antd/es/button"
+import tailwindText from "data-text:~style.css"
+import antdResetCssText from "data-text:antd/dist/reset.css"
+import { AnimatePresence, motion } from "framer-motion"
+import { useAtom } from "jotai"
+import type { PlasmoCSConfig, PlasmoGetShadowHostId } from "plasmo"
+import { FaTools } from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6"
+
+import { toolPanelOpenAtom } from "~store/app"
+import { ThemeProvider } from "~theme"
+
+import ExportFollowers from "./components/ExportFollowers"
+
+export const config: PlasmoCSConfig = {
+  matches: ["*://twitter.com/*", "*://x.com/*"]
+}
+
+const HOST_ID = "engage-csui"
+export const getShadowHostId: PlasmoGetShadowHostId = () => HOST_ID
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = tailwindText + antdResetCssText
+  return style
+}
+
+const PlasmoOverlay = () => {
+  const [open, setOpen] = useAtom(toolPanelOpenAtom)
+
+  return (
+    <ThemeProvider>
+      <StyleProvider container={document.getElementById(HOST_ID).shadowRoot}>
+        <AnimatePresence mode="sync">
+          {open ? (
+            <motion.div
+              key="panel-open"
+              layoutId="followers-panel"
+              initial={{ x: -100 }}
+              animate={{ x: 0 }}
+              exit={{ x: -100 }}
+              transition={{ duration: 0.5, ease: "linear" }}
+              className="w-80 fixed left-0 top-8">
+              <ExportFollowers />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="panel-close"
+              layoutId="followers-panel"
+              className="fixed left-0 top-8">
+              <Button
+                className="flex h-auto py-3 rounded-ss-none rounded-es-none px-2 flex-col gap-2"
+                onClick={() => setOpen(true)}>
+                <FaXTwitter size={22} />
+                <FaTools size={22} />
+              </Button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </StyleProvider>
+    </ThemeProvider>
+  )
+}
+
+export default PlasmoOverlay
